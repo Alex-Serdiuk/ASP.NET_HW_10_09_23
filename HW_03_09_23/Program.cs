36d049d3b4cc75e1c12d36061b62159624c2f705
Serdy_oj05@student.itstep.org <Serdy_oj05@student.itstep.org>
2023-09-10 23:14:29 +0300

    fixed UTF-8

--- HW_03_09_23/Program.cs
+++ HW_03_09_23/Program.cs
@@ -1,20 +1,21 @@
 using System.Xml.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
 app.Use(async (context, next) =>
 {
-    // Îòðèìóºìî ³íôîðìàö³þ ïðî êë³ºíòà
+    // Отримуємо інформацію про клієнта
     context.Items["browserInfo"] = context.Request.Headers["User-Agent"].ToString();
     context.Items["clientIp"] = context.Connection.RemoteIpAddress.ToString();
 
-    // ²íôîðìàö³ÿ ïðî ñåáå
-    context.Items["name"] = "Îëåêñ³é";
-    context.Items["surname"] = "Ñåðäþê";
-    context.Items["group"] = "ÂÏÓ011ì";
+    // Інформація про себе
+    context.Items["name"] = "Олексій";
+    context.Items["surname"] = "Сердюк";
+    context.Items["group"] = "ВПУ011м";
 
-    
+
 
     await next.Invoke();
 });
@@ -22,23 +23,23 @@ app.Use(async (context, next) =>
 
 app.MapGet("/", async (context) =>
 {
-    // Áóäóºìî HTML ñòîð³íêó
+    // Будуємо HTML сторінку
     var htmlContent = @"
         <!DOCTYPE html>
         <html lang=""en"">
         <head>
             <meta charset=""UTF-8"">
             <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
-            <title>Ìîÿ ³íôîðìàö³ÿ</title>
+            <title>Моя інформація</title>
         </head>
         <body>
-            <h1>Ìîÿ ³íôîðìàö³ÿ</h1>
-            <p>²ì'ÿ: " + context.Items["name"] + @"</p>
-            <p>Ïð³çâèùå: " + context.Items["surname"] + @"</p>
-            <p>Ãðóïà: " + context.Items["group"] + @"</p>
-            <h2>²íôîðìàö³ÿ ïðî êë³ºíòà:</h2>
-            <p>Áðàóçåð: " + context.Items["browserInfo"] + @"</p>
-            <p>IP êë³ºíòà: " + context.Items["clientIp"] + @"</p>
+            <h1>Моя інформація</h1>
+            <p>Ім'я: " + context.Items["name"] + @"</p>
+            <p>Прізвище: " + context.Items["surname"] + @"</p>
+            <p>Група: " + context.Items["group"] + @"</p>
+            <h2>Інформація про клієнта:</h2>
+            <p>Браузер: " + context.Items["browserInfo"] + @"</p>
+            <p>IP клієнта: " + context.Items["clientIp"] + @"</p>
         </body>
         </html>";
     await context.Response.WriteAsync(htmlContent);
